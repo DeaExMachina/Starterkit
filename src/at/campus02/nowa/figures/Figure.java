da7967bde449483b7d3d556fa14badbf9cabfb36
DeaExMachina <sister_of_night@gmx.at>
2022-04-06 09:12:54 +0200

    .

--- src/at/campus02/nowa/figures/Figure.java
+++ src/at/campus02/nowa/figures/Figure.java
@@ -2,17 +2,24 @@ package at.campus02.nowa.figures;
 
 public abstract class Figure {
     protected char[][] symbol = new char[3][3];
+    protected int _faktor = 3;
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                sb.append(symbol[col][row]);
+            for (int i = 0; i < _faktor; i++) {
+                for (int col = 0; col < 3; col++) {
+                    for (int j = 0; j < _faktor; j++) {
+
+                        sb.append(symbol[col][row]);
+                    }
+                }
+                sb.append('\n');
             }
-            sb.append('\n');
         }
 
         return sb.toString();
+
     }
 }
